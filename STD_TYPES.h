b220169592fcbd3b5c3c76d0158cdb56c9ffcede
Rehab Naser <rehabnaser.stf@gmail.com>
2020-07-27 21:42:12 +0200

    adding STD_TYPES.h file

---
+++ STD_TYPES.h
@@ -0,0 +1,22 @@
+/*
+ * File Name: STD_TYPES.h
+ * Creation Date: 07/20/2020
+ * Author: Rehab
+ * Description: Header File for standard data types....
+ */ 
+ 
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char uint8;
+typedef unsigned short int uint16;
+typedef unsigned long int uint32;
+
+typedef signed char int8;
+typedef signed short int int16;
+typedef signed long int int32;
+
+typedef float float32;
+typedef double double64;
+
+#endif
