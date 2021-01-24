586864832ae453eb5f745b58e05dd003b472603f
Rehab Naser <rehabnaser.stf@gmail.com>
2021-01-24 22:24:57 +0200

    adding TIMER0 folder on MCAL

---
+++ MCAL/TIMER0/TIMER0.c
@@ -0,0 +1,163 @@
+/*
+ * File Name: TIMER0.c
+ * Creation Date: 23/01/2021
+ * Author: Rehab
+ * Description: C File for TIMER0 peripheral....
+ */ 
+ 
+#include "TIMER0.h"
+
+void Timer0Init(uint8 Timer0OperationMode,uint8 Timer0ClkSource,uint8 Timer0CompareValue)
+{
+	Timer0SelectOperationMode(Timer0OperationMode,Timer0CompareValue);
+	Timer0SelectClkSource(Timer0ClkSource);
+}
+
+void Timer0SelectOperationMode(uint8 Timer0OperationMode,uint8 Timer0CompareValue)
+{
+	switch(Timer0OperationMode)
+	{
+		case TIMER0_NORMAL_MODE:
+			BIT_WRITE(TCCR_0,WGM00,BIT_LOW);
+			BIT_WRITE(TCCR_0,WGM01,BIT_LOW);
+			break;
+		case TIMER0_PHASE_CORRECT_PWM_MODE:
+			BIT_WRITE(TCCR_0,WGM00,BIT_HIGH);
+			BIT_WRITE(TCCR_0,WGM01,BIT_LOW);		
+			break;
+		case TIMER0_CTC_MODE:
+			BIT_WRITE(TCCR_0,WGM00,BIT_LOW);
+			BIT_WRITE(TCCR_0,WGM01,BIT_HIGH);		
+			break;
+		case TIMER0_FAST_PWM_MODE:
+			BIT_WRITE(TCCR_0,WGM00,BIT_HIGH);
+			BIT_WRITE(TCCR_0,WGM01,BIT_HIGH);		
+			break;
+		default:
+			break;
+	}
+	Timer0SetCompareValue(Timer0CompareValue);
+}
+
+void Timer0SelectClkSource(uint8 Timer0ClkSource)
+{
+	THREE_BITS_WRITE(TCCR_0,CS00,Timer0ClkSource);
+}
+
+void Timer0SelectCompareMatchOutputMode(uint8 Timer0WaveGeneratedMode)
+{
+	TWO_BITS_WRITE(TCCR_0,COM00,Timer0WaveGeneratedMode);
+}
+
+void Timer0SetCompareValue(uint8 Timer0CompareValue)
+{
+	REG_WRITE(OCR_0,Timer0CompareValue);
+}
+
+//Delay In msec,CTC Mode,Internal CLK
+//PrescalerValues (1,8,64,256,1024)
+/*F_CPU = 10MHZ 
+if PrescalerValue=1--> Available Delay rang is 0.0256ms(OCR0=255)-->0.0001ms(OCR0=0)
+if PrescalerValue=8--> Available Delay rang is 0.2048ms(OCR0=255)-->0.0008ms(OCR0=0)
+if PrescalerValue=64--> Available Delay rang is 1.6384ms(OCR0=255)-->0.0064ms(OCR0=0)
+if PrescalerValue=256--> Available Delay rang is 6.5536ms(OCR0=255)-->0.0256ms(OCR0=0)
+if PrescalerValue=1024--> Available Delay rang is 26.2144ms(OCR0=255)-->0.1024ms(OCR0=0)
+*/ 
+void Timer0Delay_ms(float32 Delay_ms,uint16 PrescalerValue)
+{
+	uint8 Timer0CompareValue;
+	Timer0CompareValue=(uint8)((((Delay_ms/1000.0)*F_CPU)/PrescalerValue)-1);
+	Timer0SetCompareValue(Timer0CompareValue);	
+}
+
+//Delay In sec,CTC Mode,Internal CLK
+//Recommended using Timer0CompareValue=255,PrescalerValue=1024
+uint32 Timer0Delay_sec(float32 Delay_sec,uint8 Timer0CompareValue,uint16 PrescalerValue)
+{
+	uint32 NumberOfCompareMatch;
+	float32 TimeOfOneCompareMatch=((float32)(Timer0CompareValue+1)*PrescalerValue)/F_CPU;
+	NumberOfCompareMatch=(uint32)(Delay_sec/TimeOfOneCompareMatch);
+	return NumberOfCompareMatch;
+}
+
+//Frequency In HZ,CTC Mode,Internal CLK
+//PrescalerValues (1,8,64,256,1024)
+/*F_CPU = 10MHZ 
+if PrescalerValue=1--> Available Freq rang is 19531.25Hz(OCR0=255)-->5000000Hz(OCR0=0)
+if PrescalerValue=8--> Available Freq rang is 2441.40625Hz(OCR0=255)-->625000Hz(OCR0=0)
+if PrescalerValue=64--> Available Freq rang is 305.17578125Hz(OCR0=255)-->78125Hz(OCR0=0)
+if PrescalerValue=256--> Available Freq rang is 76.2939453125Hz(OCR0=255)-->19531.25Hz(OCR0=0)
+if PrescalerValue=1024--> Available Freq rang is 19.073486328125Hz(OCR0=255)-->4882.8125Hz(OCR0=0)
+*/ 
+void Timer0SetOutputWaveFrequency_HZ(uint32 Frequency_HZ,uint16 PrescalerValue)
+{
+	uint8 Timer0CompareValue;
+	Timer0CompareValue=(uint8)((F_CPU/(2.0*Frequency_HZ*PrescalerValue))-1);
+	Timer0SetCompareValue(Timer0CompareValue);
+}
+
+//Fast PWM Mode/Phase Correct PWM Mode,Non Inverting
+void Timer0SetOutputWaveDutyCycle(uint8 DutyCycle)
+{
+	uint8 Timer0CompareValue;
+	Timer0CompareValue=(uint8)(((DutyCycle/100.0)*256.0)-1);
+	Timer0SetCompareValue(Timer0CompareValue);	
+}
+
+void Timer0AnalogWrite(float32 VoltageValue)
+{
+	uint8 DutyCycle;
+	DutyCycle=(uint8)((VoltageValue/5.0)*100);
+	Timer0SetOutputWaveDutyCycle(DutyCycle);
+}
+
+uint8 Timer0CountExternalEvent(void)
+{
+	return REG_GET(TCNT_0);
+}
+
+void Timer0SetOutputWavePin(void)
+{
+	GpioPinDirection(OC0_PORT,OC0_PIN,OUTPUT);
+}
+
+void Timer0SetExternalClkPin(void)
+{
+	GpioPinDirection(T0_PORT,T0_PIN,INPUT);
+}
+
+void Timer0InterruptInit(uint8 Timer0IntType)
+{
+	Timer0InterruptEnable(Timer0IntType); 
+	GlobalInterruptEnable();	
+}
+
+void Timer0InterruptEnable(uint8 Timer0IntType)
+{
+	switch(Timer0IntType)
+	{
+		case TIMER0_COMPARE:
+			BIT_WRITE(TIMSK_R,OCIE0,BIT_HIGH);
+			break;
+		case TIMER0_OVERFLOW:
+			BIT_WRITE(TIMSK_R,TOIE0,BIT_HIGH);
+			break;
+		default:
+			break;
+	}	
+}
+
+void Timer0InterruptDisable(uint8 Timer0IntType)
+{
+	switch(Timer0IntType)
+	{
+		case TIMER0_COMPARE:
+			BIT_WRITE(TIMSK_R,OCIE0,BIT_LOW);
+			break;
+		case TIMER0_OVERFLOW:
+			BIT_WRITE(TIMSK_R,TOIE0,BIT_LOW);
+			break;
+		default:
+			break;
+	}
+}
---
+++ MCAL/TIMER0/TIMER0.h
@@ -0,0 +1,80 @@
+/*
+ * File Name: TIMER0.h
+ * Creation Date: 23/01/2021
+ * Author: Rehab
+ * Description: Header File for TIMER0 peripheral....
+ */ 
+ 
+#ifndef TIMER0_H_
+#define TIMER0_H_
+
+//FILES INCLUDING
+#include "STD_TYPES.h"
+#include "STD_MACROS.h"
+#include "MC_REGISTERS.h"
+#include "GPIO.h"
+#include "INTERRUPT.h"
+
+//TIMER0 PORTS
+#define T0_PORT PORT_B
+#define OC0_PORT PORT_B
+
+//TIMER0 PINS
+#define T0_PIN PIN0
+#define OC0_PIN PIN3
+
+//TIMER0 OPERATION MODES
+#define TIMER0_NORMAL_MODE 0
+#define TIMER0_PHASE_CORRECT_PWM_MODE 1
+#define TIMER0_CTC_MODE 2
+#define TIMER0_FAST_PWM_MODE 3
+
+//COMPARE MATCH OUTPUT MODES(NON-PWM MODE)
+#define DISCONNECT_OC0 0
+#define TOGGLE_OC0 1
+#define CLEAR_OC0 2
+#define SET_OC0 3 
+
+//COMPARE MATCH OUTPUT MODES(FAST/PHASE CORRECT PWM MODE)
+#define NON_INVERTING_OC0_MODE 2
+#define INVERTING_OC0_MODE 3 
+
+//TIMER0 CLOCK SOURCE OPTIONS
+#define TIMER0_NO_CLK 0
+#define TIMER0_INTERNAL_CLK_NO_PRESC 1
+#define TIMER0_INTERNAL_CLK_PRESC_8 2
+#define TIMER0_INTERNAL_CLK_PRESC_64 3
+#define TIMER0_INTERNAL_CLK_PRESC_256 4
+#define TIMER0_INTERNAL_CLK_PRESC_1024 5
+#define TIMER0_EXTERNAL_CLK_FALLING_EDGE 6
+#define TIMER0_EXTERNAL_CLK_RISING_EDGE 7
+
+//TIMER0 INTERRUPTS TYPES
+#define TIMER0_COMPARE 0
+#define TIMER0_OVERFLOW 1
+
+//FUNCTIONS PROTOTYPES
+void Timer0Init(uint8 Timer0OperationMode,uint8 Timer0ClkSource,uint8 Timer0CompareValue);
+void Timer0SelectOperationMode(uint8 Timer0OperationMode,uint8 Timer0CompareValue);
+void Timer0SelectClkSource(uint8 Timer0ClkSource);
+void Timer0SelectCompareMatchOutputMode(uint8 Timer0WaveGeneratedMode);
+void Timer0SetCompareValue(uint8 Timer0CompareValue);
+
+void Timer0Delay_ms(float32 Delay_ms,uint16 PrescalerValue);//Delay In msec,CTC Mode,Internal CLK
+uint32 Timer0Delay_sec(float32 Delay_sec,uint8 Timer0CompareValue,uint16 PrescalerValue);//Delay In sec,CTC Mode,Internal CLK
+
+void Timer0SetOutputWaveFrequency_HZ(uint32 Frequency_HZ,uint16 PrescalerValue);//Frequency In HZ,CTC Mode,Internal CLK
+
+void Timer0SetOutputWaveDutyCycle(uint8 DutyCycle);//Fast PWM Mode/Phase Correct PWM Mode,Non Inverting
+void Timer0AnalogWrite(float32 VoltageValue);
+
+uint8 Timer0CountExternalEvent(void);
+
+void Timer0SetOutputWavePin(void);
+void Timer0SetExternalClkPin(void);
+
+void Timer0InterruptInit(uint8 Timer0IntType);
+void Timer0InterruptEnable(uint8 Timer0IntType);
+void Timer0InterruptDisable(uint8 Timer0IntType);
+
+#endif
