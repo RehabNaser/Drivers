95ff78b76f5f7cada804c205eaa9225baeb1223f
Rehab Naser <rehabnaser.stf@gmail.com>
2021-02-05 16:30:43 +0200

    adding UART folder on MCAL

---
+++ MCAL/UART/UART.c
@@ -0,0 +1,174 @@
+/*
+ * File Name: UART.c
+ * Creation Date: 02/02/2021
+ * Author: Rehab
+ * Description: C File for UART peripheral....
+ */ 
+ 
+#include "UART.h"
+
+void UARTInit(uint32 BaudRate)
+{
+	UARTSetStopBitSize(ONE_STOP_BIT);
+	UARTSetParityBit(NO_PARITY);
+	UARTSetDataSize(EIGHT_BITS);
+	UARTSetBaudRate(BaudRate,SINGLE_SPEED);
+	UARTEnableTX();
+	UARTEnableRX();
+}
+
+void UARTCustomInit(uint32 BaudRate,uint8 AsyncSpeedMode,uint8 DataSize,uint8 UARTParityMode,uint8 UARTStopBitSize)
+{
+	UARTSetStopBitSize(UARTStopBitSize);
+	UARTSetParityBit(UARTParityMode);
+	UARTSetDataSize(DataSize);
+	UARTSetBaudRate(BaudRate,AsyncSpeedMode);
+	UARTEnableTX();
+	UARTEnableRX();
+}
+
+void UARTEnableTX(void)
+{
+	BIT_WRITE(UCS_RB,TXEN,BIT_HIGH);
+}
+
+void UARTEnableRX(void)
+{
+	BIT_WRITE(UCS_RB,RXEN,BIT_HIGH);
+}
+
+void UARTDisableTX(void)
+{
+	BIT_WRITE(UCS_RB,TXEN,BIT_LOW);
+}
+
+void UARTDisableRX(void)
+{
+	BIT_WRITE(UCS_RB,RXEN,BIT_LOW);
+}
+
+void UARTSetParityBit(uint8 UARTParityMode)
+{
+	TWO_BITS_WRITE(UCS_RC,UPM0,UARTParityMode);
+	BIT_WRITE(UCS_RC,URSEL,BIT_HIGH);
+}
+
+void UARTSetStopBitSize(uint8 UARTStopBitSize)
+{
+	BIT_WRITE(UCS_RC,USBS,UARTStopBitSize);
+	BIT_WRITE(UCS_RC,URSEL,BIT_HIGH);
+}
+
+void UARTSetDataSize(uint8 DataSize)
+{
+	TWO_BITS_WRITE(UCS_RC,UCSZ0,TWO_BITS_GET(DataSize,0));
+	BIT_WRITE(UCS_RC,URSEL,BIT_HIGH);
+	BIT_WRITE(UCS_RB,UCSZ2,(DataSize>>2));
+}
+
+void UARTSetBaudRate(uint32 BaudRate,uint8 AsyncSpeedMode)
+{
+	BIT_WRITE(UCS_RA,U2X,AsyncSpeedMode);
+	uint16 UBRRValue;
+	switch(AsyncSpeedMode)
+	{
+		case SINGLE_SPEED:
+			UBRRValue=(uint16)((F_CPU/(16.0*BaudRate))-1);
+			break;
+		case DOUBLE_SPEED:
+			UBRRValue=(uint16)((F_CPU/(8.0*BaudRate))-1);
+			break;
+		default:
+			break;		
+	}
+	REG_WRITE(UBR_RH,(UBRRValue>>8));
+	REG_WRITE(UBR_RL,UBRRValue);
+}
+
+void UARTTransmit(uint8 Data)
+{
+	while(!(BIT_GET(UCS_RA,UDRE)));
+	REG_WRITE(UD_R,Data);
+}
+
+void UARTTransmitString(uint8 *Data)
+{
+	for(uint8 i=0;Data[i]!='\0';i++)
+	{
+		UARTTransmit(Data[i]);
+	}
+}
+
+void UARTTransmit9Bits(uint16 Data)
+{
+	while(!(BIT_GET(UCS_RA,UDRE)));
+	BIT_WRITE(UCS_RB,TXB8,BIT_GET((Data>>8),0));
+	REG_WRITE(UD_R,Data);
+}
+
+uint8 UARTAvailable(void)
+{
+	if(BIT_GET(UCS_RA,RXC))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+uint8 UARTReceive(void)
+{
+	return REG_GET(UD_R);
+}
+
+uint16 UARTReceive9Bits(void)
+{
+	uint16 Data;
+	Data =(BIT_GET(UCS_RB,RXB8)>>RXB8)<<8;
+	Data |=REG_GET(UD_R);
+	return Data;
+}
+
+void UARTInterruptInit(uint8 UARTIntType)
+{
+	UARTInterruptEnable(UARTIntType);
+	GlobalInterruptEnable();
+}
+
+void UARTInterruptEnable(uint8 UARTIntType)
+{
+	switch(UARTIntType)
+	{
+		case RX_COMPLETE:
+			BIT_WRITE(UCS_RB,RXCIE,BIT_HIGH);
+			break;
+		case TX_COMPLETE:
+			BIT_WRITE(UCS_RB,TXCIE,BIT_HIGH);
+			break;
+		case DATA_REGISTER_EMPTY:
+			BIT_WRITE(UCS_RB,UDRIE,BIT_HIGH);
+			break;
+		default:
+			break;
+	}	
+}
+
+void UARTInterruptDisable(uint8 UARTIntType)
+{
+	switch(UARTIntType)
+	{
+		case RX_COMPLETE:
+			BIT_WRITE(UCS_RB,RXCIE,BIT_LOW);
+			break;
+		case TX_COMPLETE:
+			BIT_WRITE(UCS_RB,TXCIE,BIT_LOW);
+			break;
+		case DATA_REGISTER_EMPTY:
+			BIT_WRITE(UCS_RB,UDRIE,BIT_LOW);
+			break;
+		default:
+			break;
+	}		
+}
---
+++ MCAL/UART/UART.h
@@ -0,0 +1,71 @@
+/*
+ * File Name: UART.h
+ * Creation Date: 02/02/2021
+ * Author: Rehab
+ * Description: Header File for UART peripheral....
+ */ 
+ 
+#ifndef UART_H_
+#define UART_H_
+
+//FILES INCLUDING
+#include "STD_TYPES.h"
+#include "STD_MACROS.h"
+#include "MC_REGISTERS.h"
+#include "INTERRUPT.h"
+
+//UART INTERRUPTS TYPES
+#define RX_COMPLETE 0
+#define TX_COMPLETE  1
+#define DATA_REGISTER_EMPTY 2
+
+//UART ASYNC. SPEED TYPES
+#define SINGLE_SPEED 0
+#define DOUBLE_SPEED 1
+
+//UART DATA SIZE
+#define FIVE_BITS 0
+#define SIX_BITS 1
+#define SEVEN_BITS 2
+#define EIGHT_BITS 3
+#define NINE_BITS 7
+
+//UART STOP BITS SIZE
+#define ONE_STOP_BIT 0
+#define TWO_STOP_BIT 1
+
+//UART PARITY MODES
+#define NO_PARITY 0
+#define EVEN_PARITY 2
+#define ODD_PARITY 3
+
+//USART MODES
+#define ASYNCHRONOUS 0
+#define SYNCHRONOUS 1
+
+//FUNCTIONS PROTOTYPES
+void UARTInit(uint32 BaudRate);
+void UARTCustomInit(uint32 BaudRate,uint8 AsyncSpeedMode,uint8 DataSize,uint8 UARTParityMode,uint8 UARTStopBitSize);
+
+void UARTEnableTX(void);
+void UARTEnableRX(void);
+void UARTDisableTX(void);
+void UARTDisableRX(void);
+
+void UARTSetParityBit(uint8 UARTParityMode);
+void UARTSetStopBitSize(uint8 UARTStopBitSize);
+void UARTSetDataSize(uint8 DataSize);
+void UARTSetBaudRate(uint32 BaudRate,uint8 AsyncSpeedMode);
+
+void UARTTransmit(uint8 Data);
+void UARTTransmitString(uint8 *Data);
+void UARTTransmit9Bits(uint16 Data);//Data values from 0 to 511
+uint8 UARTAvailable(void);
+uint8 UARTReceive(void);
+uint16 UARTReceive9Bits(void);
+
+void UARTInterruptInit(uint8 UARTIntType);
+void UARTInterruptEnable(uint8 UARTIntType);
+void UARTInterruptDisable(uint8 UARTIntType);
+
+#endif
